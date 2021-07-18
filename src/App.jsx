b16cdd08f62c70de2aa2dfd208a093ae49124e37
Akarsh Lahariya <akarshlahariya1234@gmail.com>
2021-07-18 14:29:19 -0400

    Poster

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
-//eslint-disable-next-line 
 import { Route } from "react-router-dom";
 
-//HOC
+// HOC
 import DefaultHOC from "./HOC/Default.HOC";
+import MovieHOC from "./HOC/Movie.HOC";
 
-//Pages
+// Pages
 import HomePage from "./pages/Home.page";
+import Movie from "./pages/Movie.page";
+import Plays from "./pages/Plays.page";
 
-//Components
-import Temp from "./components/temp.jsx"; 
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-//JSX
 function App() {
   return (
     <>
-      <DefaultHOC path = "/" exact component={HomePage} />
+      <DefaultHOC path="/" exact component={HomePage} />
+      <MovieHOC path="/movie/:id" exact component={Movie} />
+      <DefaultHOC path="/plays" exact component={Plays} />
     </>
-  ); 
+  );
 }
 
 export default App;
--- src/components/HeroCarousal/Arrows.component.jsx
+++ src/components/HeroCarousal/Arrows.component.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export const NextArrow = (props) => {
-    return ( 
-    <> 
-       <div className={props.classNames}
-        style={{ ...props.style, backgroundColor: "black"}}
-        onClick={props.onClick} 
-       />
+  return (
+    <>
+      <div
+        className={props.className}
+        style={{ ...props.style, backgroundColor: "black" }}
+        onClick={props.onClick}
+      />
     </>
-    );
+  );
 };
 
-export  const PrevArrow = (props) => {
-    return (
-        <>
-            <div className={props.classNames}
-            style={{ ...props.style, backgroundColor: "black"}}
-            onClick={props.onClick} 
-            />
-        </> 
-    );
+export const PrevArrow = (props) => {
+  return (
+    <>
+      <div
+        className={props.className}
+        style={{ ...props.style, backgroundColor: "black" }}
+        onClick={props.onClick}
+      />
+    </>
+  );
 };
